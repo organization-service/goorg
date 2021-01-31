317b095ae264e3561cc3365474b9fc3819ae275e
ncs-nozomi-nishinohara <nozomi_nishinohara@n-creativesystem.com>
2021-01-31 07:03:53 +0000

    リポジトリ改修

--- repository/repository.go
+++ repository/repository.go
@@ -8,7 +8,7 @@ type (
 		MustConnection() Connection
 	}
 	Connection interface {
-		Transaction(c context.Context, f func(tx Transaction) error) error
+		Transaction(c context.Context, f func(tx interface{}) error) error
 	}
 	Transaction interface {
 	}
