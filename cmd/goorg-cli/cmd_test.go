a3f4c77fc137af307b584f1dd8f8acd4c80832f2
ncs-nozomi-nishinohara <nozomi_nishinohara@n-creativesystem.com>
2021-02-17 07:58:27 +0000

    cmd系改修

---
+++ cmd/goorg-cli/cmd_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+import (
+	"testing"
+
+	swaggerGene "github.com/organization-service/goorg/cmd/goorg-cli/swagger-generator"
+)
+
+func TestSwagger(t *testing.T) {
+	swaggerGene.Action()
+}
